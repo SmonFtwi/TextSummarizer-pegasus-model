8ab5e739ecf47370d857fbfa4a605a10ea96a014
SmonFtwi <73071179+SmonFtwi@users.noreply.github.com>
2023-03-21 14:56:21 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
 import json
 import pickle
 import streamlit as st
-from streamlit_option_menu import option_menu
+
 import pandas as pd
 import numpy as np
 import streamlit as st
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 #import summarize.ipynp
 
-option_menu(
-    menu_title=None,
-    options=['Home', 'Our Porject', 'About US'],
-    icons=['house', 'book', 'envelop'],
 
-)
 
 
 st.title('Text summurization ')
 
 
-hide_st_style = """ 
-         <style>
-             footer{ visiblity: hidden}
-             header{ visibility: hidden}
-             #MainMenu {visibility: hidden}
-         </style>
-"""
 
-st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
 
 
 text_input = st.text_input("Enter some text:")
